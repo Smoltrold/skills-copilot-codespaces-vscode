4c71ac54cb8e4c5a16a5f0c6d8a9903fbedd2124
Smoltrold <139801336+Smoltrold@users.noreply.github.com>
2023-07-19 12:31:57 +0000

    GitP

--- comment.js
+++ comment.js
@@ -1,1 +1,21 @@
-// Create web server    
+// Create web server
+control 
+    .get('/comment', (req, res) => {    
+
+        // Get all comments
+        Comment.find()
+            .then(comments => {
+                res.json(comments);
+            })
+            .catch(err => {
+                res.json(err);
+            
+            });
+        })
+{
+    git add comments.js
+
+    git commit -m Copilot third commit
+
+    git push    
+}   
