5e3489fd40c96642f9e1060d4df96fff52f04fb0
Smoltrold <139801336+Smoltrold@users.noreply.github.com>
2023-07-19 11:59:19 +0000

    SecondCommit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    scope: {
+      member: '='
+    }
+  };
+}
+{
+    git add member.js
+    
+    git commit -m "Copilot second commit"
+    
+    git push   
+} 
+   
